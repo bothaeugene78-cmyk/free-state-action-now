47b3c2c8911eb706d9b934618b61a7b8ad02b635
bothaeugene78-cmyk <botha.eugene78@gmail.com>
2026-05-18 22:50:01 +0200

    Add src/api/entities.js

---
+++ src/api/entities.js
@@ -0,0 +1,17 @@
+import { base44 } from './base44Client';
+
+export const Municipality = base44.entities["Municipality"];
+export const OfficialContact = base44.entities["OfficialContact"];
+export const ProvincialContact = base44.entities["ProvincialContact"];
+export const EmergencyContact = base44.entities["EmergencyContact"];
+export const Report = base44.entities["Report"];
+export const EvidenceItem = base44.entities["EvidenceItem"];
+export const EscalationEvent = base44.entities["EscalationEvent"];
+export const LegalNotice = base44.entities["LegalNotice"];
+export const Comment = base44.entities["Comment"];
+export const Vote = base44.entities["Vote"];
+export const NewsItem = base44.entities["NewsItem"];
+export const LegalDevelopment = base44.entities["LegalDevelopment"];
+export const ModerationFlag = base44.entities["ModerationFlag"];
+export const MonitorRunLog = base44.entities["MonitorRunLog"];
+export const TemplateApproval = base44.entities["TemplateApproval"];
